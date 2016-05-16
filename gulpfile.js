3a63eab62731b0c2dc3e7b8a12cf4e850fe3c7af
utsabneupane <utsabneupane@lftechnology.com>
2016-05-16 13:41:38 +0545

    implement the various plugin installed

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,39 @@
-var gulp= require('gulp');
-gulp.task('default',function(){
-	console.log("Hello World");
-})
+////////////////////////
+// Required
+///////////////////////
+
+var gulp= require('gulp'),
+uglify = require('gulp-uglify'),
+rename= require('gulp-rename');
+
+////////////////////////
+// Scripts task
+///////////////////////
+
+gulp.task('scripts',function(){
+	gulp.src(['app/js/**/*.js','!app/js/**/*.min.js'])
+	.pipe(rename({suffix:'.min'}))
+	.pipe(uglify())
+	.pipe(gulp.dest('app/js'));
+});
+
+
+////////////////////////
+// Html task
+///////////////////////
+gulp.task('html',function(){
+	gulp.src('app/**/*.html');
+});
+
+////////////////////////
+// Watch task
+///////////////////////
+gulp.task('watch',function(){
+	gulp.watch('app/js/**/*.js',['scripts']);
+	gulp.watch('app/**/*.html',['html']);
+});
+
+////////////////////////
+// Default task
+///////////////////////
+gulp.task('default',['scripts','html','watch']);
