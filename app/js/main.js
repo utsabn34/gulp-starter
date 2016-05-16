d4632c2b664604eacc130f553d21c3c8cafc4207
utsabneupane <utsabneupane@lftechnology.com>
2016-05-16 13:42:27 +0545

    create test file to test various plugin

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+console.log("Testing uglification");
+console.log("again doing it ");
+console.log("with new requirement");
+console.log("are you comedy me");
+console.log("when did this happen");
+console.log("how did this happens");
