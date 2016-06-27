46a7a78118ae595753dbac7da23effc1fdb8d290
utsabneupane <utsabneupane@lftechnology.com>
2016-06-27 08:49:24 +0545

    #gulp-concat-plugin-impl: implement concat plugin for concatinating css and js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,25 @@
-var gulp= require('gulp');
-gulp.task('default',function(){
-	console.log("Hello World");
-})
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+gulp.task('sass', function() {
+    return gulp.src('app/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('src/css/'));
+});
+
+gulp.task('concat-css', function() {
+    return gulp.src('app/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(concat('compiled.scss'))
+        .pipe(gulp.dest('src/css/'));
+});
+
+gulp.task('concat-js', function() {
+    return gulp.src('app/js/**/*.js')
+        .pipe(concat('compiled.js'))
+        .pipe(gulp.dest('src/js/'));
+});
+
+// Default task
+gulp.task('default',['sass','concat-css','concat-js']);
---
+++ src/js/compiled.js
@@ -0,0 +1,20 @@
+;
+(function() {
+    'use strict';
+
+    function helloWorld() {
+        console.log('Hello World !!!!!');
+    }
+
+    helloWorld();
+})();
+;
+(function() {
+    'use strict';
+
+    function newWorld() {
+        console.log('New World !!!!');
+    }
+
+    newWorld();
+})();
